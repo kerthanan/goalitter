5b16e8f1cb5b2f7fbaf8902f8f400c1c1ddcdbd0
Kerthanan <kerthanan20@gmail.com>
2022-10-09 17:45:32 -0400

    Edited the styling and filled in image placeholder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_011322) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_08_175824) do
   create_table "goals", force: :cascade do |t|
     t.text "goal"
     t.datetime "created_at", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_011322) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "username"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
